7b3846ea2ff09ca399f79b533b3cecae977057e5
freref <35976402+freref@users.noreply.github.com>
2025-09-08 15:40:27 +0200

    readme: add po2 info

--- build.zig
+++ build.zig
@@ -4,9 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const unmanaged_any_spsc_mod = b.addModule("unmanaged_spsc_queue", .{
+        .root_source_file = b.path("src/SpscQueueAnyUnmanaged.zig"),
+        .target = target,
+    });
+
+    const unmanaged_po2_spsc_mod = b.addModule("unmanaged_any_spsc_queue", .{
+        .root_source_file = b.path("src/SpscQueuePo2Unmanaged.zig"),
+        .target = target,
+    });
+
     const unmanaged_spsc_mod = b.addModule("unmanaged_spsc_queue", .{
         .root_source_file = b.path("src/SpscQueueUnmanaged.zig"),
         .target = target,
+        .imports = &.{
+            .{ .name = "unmanaged_any_spsc_queue", .module = unmanaged_any_spsc_mod },
+            .{ .name = "unmanaged_po2_spsc_queue", .module = unmanaged_po2_spsc_mod },
+        },
     });
 
     const managed_spsc_mod = b.addModule("managed_spsc_queue", .{
